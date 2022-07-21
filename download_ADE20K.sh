a4797ee437824587c126a605493b405d0f644b18
CzJaewan <wodhks7968@gmail.com>
2022-07-21 19:27:49 +0900

    add download_ade20k

---
+++ download_ADE20K.sh
@@ -0,0 +1,4 @@
+wget -O ./datasets/data/ade20k/ADEChallengeData2016.zip http://data.csail.mit.edu/places/ADEchallenge/ADEChallengeData2016.zip
+unzip ./datasets/data/ade20k/ADEChallengeData2016.zip -d ./data
+rm ./datasets/data/ade20k/ADEChallengeData2016.zip
+echo "Dataset downloaded."
